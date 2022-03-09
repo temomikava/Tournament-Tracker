ddb416ab0a122e1f0123691e03ef1cb7bac4ca22
temo mikava <temsonmikva@gmail.com>
2022-03-09 12:26:11 +0400

    class library creation

---
+++ TournamentTracker/TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    /// <summary>
+    /// Represents one match in the tournament.
+    /// </summary>
+    public class MatchupModel
+    {
+        /// <summary>
+        /// The set of team that were involved in the match.
+        /// </summary>
+        public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+        /// <summary>
+        /// The winner of the match.
+        /// </summary>
+        public TeamModel Winner { get; set; }
+        /// <summary>
+        /// Which round this match is a part of.
+        /// </summary>
+        public int MatchupRound { get; set; }
+    }
+}
