bf715337ccb9b422fc6fcb8197c179ccd9ec89d5
temo mikava <temsonmikva@gmail.com>
2022-03-11 14:52:38 +0400

    Text Connection

--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
-
+using TrackerLibrary.DataAccess.TextHelpers;
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
         //TODO - Wire up the CreatePrize method for text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            //Load the text file and convert the text to List<PrizeModel>
+            List<PrizeModel> Prizes=PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+            //Find the max ID
+            int currentId = 1;
+            if (Prizes.Count()>0)
+            {
+                currentId= Prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id=currentId;
+            //Add the new record with the new ID(ID=max+1)
+            Prizes.Add(model);
+            //Convert the prizes to list<string>
+            //Save the list<string> to the text file
+            Prizes.SaveToPrizeFile(PrizesFile);
+
             return model;
         }
     }
---
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess.TextHelpers
+{
+    //Load the text file
+    //Convert the text to List<PrizeModel>
+    //Find the max ID
+    //Add the new record with the new ID(ID=max+1)
+    //Convert the prizes to list<string>
+    //Save the list<string> to the text file
+    public static class TextConnectorProcessor
+    {
+        public static string FullFilePath(this string fileName)
+        {
+            return $"{ConfigurationManager.AppSettings["filePath"]}\\{fileName}";
+        }
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+            return File.ReadAllLines(file).ToList();
+        }
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+            foreach (string line in lines)
+            {
+                string[] columns=line.Split(',');
+                PrizeModel prizeModel = new PrizeModel();
+                prizeModel.Id = int.Parse(columns[0]);
+                prizeModel.PlaceNumber=int.Parse(columns[1]);
+                prizeModel.PlaceName= columns[2];
+                prizeModel.PrizeAmount=decimal.Parse(columns[3]);
+                prizeModel.PrizePercentage = double.Parse(columns[4]);
+                output.Add(prizeModel);
+            }
+            return output;
+        }
+        public static void SaveToPrizeFile(this List<PrizeModel> models, string fileName)
+        {
+            List<string>lines=new List<string>();
+            foreach (PrizeModel p in models)
+            {
+                lines.Add($"{p.Id },{ p.PlaceNumber },{ p.PlaceName },{ p.PrizeAmount },{ p.PrizePercentage}");
+            }
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+        }
+
+    }
+}
